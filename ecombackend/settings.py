ce5bdfb59265b77fed68dba588bb6bfcff8883e3
Tony-Omondi <otienotony598@gmail.com>
2025-07-22 15:49:17 +0300

    home is working

--- ecombackend/settings.py
+++ ecombackend/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = 'django-insecure-l&(qvuht^sapqi^(z94((yx+cejdygacq39ctie04l-9fa5$0z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.88.85', '127.0.0.1', '192.168.100.234', '10.35.136.162', '54.146.190.174']
+ALLOWED_HOSTS = ['192.168.88.85', '127.0.0.1', '192.168.100.234', '10.35.136.162', '192.168.100.63']
 
 # Application definition
 INSTALLED_APPS = [
